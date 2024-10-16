d09a48294f9d5e3af05b1021f85215071172365d
MOHAMMED USMAN <91519976+MOHAMMED-USMAN1@users.noreply.github.com>
2024-10-16 15:00:09 +0530

    ruleengineast.py

---
+++ ruleengineast.py
@@ -0,0 +1,12 @@
+#datastrucutre
+# ast_engine.py
+
+class Node:
+    def __init__(self, node_type, value=None, left=None, right=None):
+        self.type = node_type  # "operator" (AND/OR) or "operand" (condition)
+        self.value = value      # For operands (like age, department)
+        self.left = left        # Left child node
+        self.right = right      # Right child node
+
+    def __repr__(self):
+        return f"Node(type={self.type}, value={self.value})"
